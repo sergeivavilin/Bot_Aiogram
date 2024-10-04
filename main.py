06cfeeb11ce27f2b2d557201bcad938fe172612f
Sergei <vavilin.dev@gmail.com>
2024-10-04 14:39:06 +0300

    create base router, added base handlers

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 import asyncio
 
-from aiogram import Bot, Dispatcher, Router, types
+from aiogram import Bot, Dispatcher, Router
 from aiogram.fsm.storage.memory import MemoryStorage
-from aiogram.client.default import DefaultBotProperties
-from aiogram.enums import ParseMode
-from  aiogram.types import Message
+
+from aiogram.types import Message
+from aiogram.filters import CommandStart
+
+# Для хранения и настройки чувствительных данных используем библиотеку decouple
+# Можно использовать dotenv или просто переменные окружения для хранения токена
 from decouple import config
 
-
-bot = Bot(token=config("BOT_TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+# Создаем бота, токен берем из файла .env
+bot = Bot(token=config("BOT_TOKEN"))
+# инициализируем диспатчер
 dp = Dispatcher(storage=MemoryStorage())
+# Создаем базовый роутер (в версии 3.x используется Router() вместо dp.message_handler)
+base_router = Router()
 
+# Класс CommandStart обрабатывает только команду /start.
+@base_router.message(CommandStart())
+async def start(message: Message):
+    print(f"Привет! {message.from_user.full_name}! Я бот помогающий твоему здоровью.")
 
+# Если оставить роутер без аргументов, то он будет отлавливать все необработанные ранее сообщения
+@base_router.message()
+async def all_massages(message: Message):
+    print(f"Введите команду /start, чтобы начать общение.")
+
+# Основная асинхронная функция. Добавляем роутер в диспатчер и запускаем его.
 async def main():
+    dp.include_router(base_router)
     await dp.start_polling(bot)
+
 
 if __name__ == '__main__':
     asyncio.run(main())
